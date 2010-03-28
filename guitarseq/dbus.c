3563612e9c52ea9d6778c1a3e8f9caead9b4f436
Alexandru Stan <alex@hypertriangle.com>
2010-03-27 21:22:40 -0400

    tried some more dbus stuff

--- guitarseq/dbus.c
+++ guitarseq/dbus.c
@@ -1,43 +1,32 @@
 #include <dbus/dbus-glib.h>
 #include <glib-object.h>
+#include <stdio.h>
 
 #include "dbus.h"
 
-int dbusTest (void)
+DBusGConnection *dbus_connection;
+GError *dbus_error;
+
+int dbus_list (void)
 {
-	DBusGConnection *connection;
-	GError *error;
 	DBusGProxy *proxy;
 	char **name_list;
 	char **name_list_ptr;
 	
-	g_type_init ();
-	
-	error = NULL;
-	connection = dbus_g_bus_get (DBUS_BUS_SESSION, &error);
-	if (connection == NULL) {
-		g_printerr ("Failed to open connection to bus: %s\n",
-		error->message);
-		g_error_free (error);
-		exit (1);
-	}
-	
 	/* Create a proxy object for the "bus driver" (name "org.freedesktop.DBus") */
 	
-	proxy = dbus_g_proxy_new_for_name (connection, DBUS_SERVICE_DBUS, DBUS_PATH_DBUS, DBUS_INTERFACE_DBUS);
+	proxy = dbus_g_proxy_new_for_name (dbus_connection, DBUS_SERVICE_DBUS, DBUS_PATH_DBUS, DBUS_INTERFACE_DBUS);
 	
 	/* Call ListNames method, wait for reply */
-	error = NULL;
-	if (!dbus_g_proxy_call (proxy, "ListNames", &error, G_TYPE_INVALID,G_TYPE_STRV, &name_list, G_TYPE_INVALID)) {
+	dbus_error = NULL;
+	if (!dbus_g_proxy_call (proxy, "ListNames", &dbus_error, G_TYPE_INVALID,G_TYPE_STRV, &name_list, G_TYPE_INVALID)) {
 		/* Just do demonstrate remote exceptions versus regular GError */
-		if (error->domain == DBUS_GERROR && error->code == DBUS_GERROR_REMOTE_EXCEPTION)
-			g_printerr ("Caught remote method exception %s: %s",
-			dbus_g_error_get_name (error),
-			error->message);
+		if (dbus_error->domain == DBUS_GERROR && dbus_error->code == DBUS_GERROR_REMOTE_EXCEPTION)
+			g_printerr ("Caught remote method exception %s: %s", dbus_g_error_get_name (dbus_error), dbus_error->message);
 		else
-			g_printerr ("Error: %s\n", error->message);
-		g_error_free (error);
-		exit (1);
+			g_printerr ("Error: %s\n", dbus_error->message);
+		g_error_free (dbus_error);
+		return 1;
 	}
 	
 	/* Print the results */
@@ -48,6 +37,24 @@ int dbusTest (void)
 	g_strfreev (name_list);
 	
 	g_object_unref (proxy);
+	
+	return 0;
+}
+
+int dbus_init() {
+	g_type_init ();
+	
+	dbus_error = NULL;
+	dbus_connection = dbus_g_bus_get (DBUS_BUS_SESSION, &dbus_error);
+	if (dbus_connection == NULL) {
+		g_printerr ("Failed to open connection to bus: %s\n", dbus_error->message);
+		g_error_free (dbus_error);
+		return 1;
+	}
+	fprintf(stdout,"DBus connection established.\n");
+	
+	//dbus_list();
+	dbus_g_object_type_install_info (NULL, dbus_glib_guitarseq_object_info);
 	
 	return 0;
 }
