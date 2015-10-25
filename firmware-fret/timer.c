7edbdeb44ede82842a879ae43f348b5e534068f1
Alexandru M Stan <alex@hypertriangle.com>
2015-10-25 00:28:11 -0700

    firmware-fret: working touch sensor

---
+++ firmware-fret/timer.c
@@ -0,0 +1,42 @@
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
+#include <libopencm3/cm3/nvic.h>
+#include <libopencm3/cm3/systick.h>
+
+/* monotonically increasing number of milliseconds from reset
+ * overflows every 49 days if you're wondering
+ */
+volatile uint32_t system_millis;
+
+/* Called when systick fires */
+void sys_tick_handler(void)
+{
+	system_millis++;
+
+	if ((system_millis % 100) == 0)
+		fflush(0);
+}
+
+/* sleep for delay milliseconds */
+static void msleep(uint32_t delay)
+{
+	uint32_t wake = system_millis + delay;
+	while (wake > system_millis);
+}
+
+/* Set up a timer to create 1mS ticks. */
+static void systick_setup(void)
+{
+	/* clock rate / 1000 to get 1mS interrupt rate */
+	systick_set_reload(48000);
+	systick_set_clocksource(STK_CSR_CLKSOURCE_AHB);
+	systick_counter_enable();
+	/* this done last */
+	systick_interrupt_enable();
+}
+
+/* set STM32 to clock by 48MHz from HSI oscillator */
+static void clock_setup(void)
+{
+	rcc_clock_setup_in_hsi_out_48mhz();
+}
