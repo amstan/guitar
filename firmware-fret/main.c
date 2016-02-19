2c17586994627f6bf1d54f7c61777c09ab9a68a6
Alexandru M Stan <alex@hypertriangle.com>
2016-02-19 00:13:46 -0800

    firmware-fret: remove touchpad stuff for now

--- firmware-fret/main.c
+++ firmware-fret/main.c
@@ -36,18 +36,6 @@
 #define LED_ORANGE (GPIOC, GPIO8)
 #define LED_GREEN  (GPIOC, GPIO9)
 
-#define S1_PORT GPIOA
-#define S1_P GPIO2 //TS_G1_IO3
-#define S1_C GPIO3 //TS_G1_IO4
-
-#define S2_PORT GPIOA
-#define S2_P GPIO6 //TS_G2_IO3
-#define S2_C GPIO7 //TS_G2_IO4
-
-#define S3_PORT GPIOB
-#define S3_P GPIO0 //TS_G3_IO2
-#define S3_C GPIO1 //TS_G3_IO3
-
 static void gpio_setup(void)
 {
 	rcc_periph_clock_enable(RCC_GPIOA);
@@ -97,32 +85,6 @@ void usart_setup(void)
 	usart_enable(USART1);
 }
 
-int read_touch(void) {
-
-// 	gpio_clear(S2_PORT, S2_C);
-// 	gpio_mode_setup(S2_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, S2_C);
-// 	msleep(50);
-// 	gpio_mode_setup(S2_PORT, GPIO_MODE_INPUT, GPIO_PUPD_NONE, S2_C);
-
-	gpio_mode_setup(S1_PORT, GPIO_MODE_AF, GPIO_PUPD_NONE, S1_C | S1_P);
-	gpio_set_af(S1_PORT, GPIO_AF3, S1_C | S1_P);
-
-	gpio_mode_setup(S2_PORT, GPIO_MODE_AF, GPIO_PUPD_NONE, S2_C | S2_P);
-	gpio_set_af(S2_PORT, GPIO_AF3, S2_C | S2_P);
-
-	gpio_mode_setup(S3_PORT, GPIO_MODE_AF, GPIO_PUPD_NONE, S3_C | S3_P);
-	gpio_set_af(S3_PORT, GPIO_AF3, S3_C | S3_P);
-
-	TSC_CR |= TSC_CR_START;
-	while(TSC_CR & TSC_CR_START);
-
-// 	while((TSC_IOGCSR & (TSC_IOGCSR_GxS(1) | TSC_IOGCSR_GxS(2) | TSC_IOGCSR_GxS(3))) != (TSC_IOGCSR_GxS(1) | TSC_IOGCSR_GxS(2) | TSC_IOGCSR_GxS(3)));
-
-	return 0;
-}
-
-uint16_t history[1000][3];
-
 int main(void)
 {
 	clock_setup();
@@ -130,83 +92,13 @@ int main(void)
 	usart_setup();
 	gpio_setup();
 
-	uint16_t max[3];
-	uint16_t value[3];
-
-// 	for (uint16_t h = 0; h < 1000; h++) {
-// 		for (uint8_t i = 0; i < 3; i++) {
-// 			history[h][i] = 0
-// 		}
-// 	}
-
 	gpio_set LED_GREEN;
-// 	printf("\n\nGuitar Fret Discovery Board\n");
+	printf("\n\nGuitar Fret Discovery Board\n");
 	msleep(100);
 
-	rcc_periph_clock_enable(RCC_TSC);
-
-	TSC_CR |= 0b1111 << TSC_CR_CTPH_SHIFT; //charge transfer pulse high
-	TSC_CR |= 0b1111 << TSC_CR_CTPL_SHIFT; //charge transfer pulse low
-	TSC_CR |= 0b1111111 << TSC_CR_SSD_SHIFT; //spread spectrum deviation
-	TSC_CR |= 0b110 << TSC_CR_MCV_SHIFT; //max count value
-	TSC_CR |= 0b000 << TSC_CR_PGPSC_SHIFT; //clock prescaler
-
-	TSC_IOSCR |= TSC_IOSCR_G1(4) | TSC_IOSCR_G2(4) | TSC_IOSCR_G3(3); //sampling capacitors selection per group
-	TSC_IOCCR |= TSC_IOCCR_G1(3) | TSC_IOCCR_G2(3) | TSC_IOCCR_G3(2); //channel selection per grou
-
-	TSC_IOGCSR |= TSC_IOGCSR_GxE(1) | TSC_IOGCSR_GxE(2) | TSC_IOGCSR_GxE(3); //enable aquision on the channels
-	//statuses: TSC_IOGCSR_GxS(1) | TSC_IOGCSR_GxS(2) | TSC_IOGCSR_GxS(3);
-
-	TSC_CR |= TSC_CR_TSCE; //enable touch sensing controller
-
-	while (1) {
-		uint8_t valid = 1;
-		gpio_toggle LED_BLUE;
-
-		read_touch();
-
-		for (unsigned int i = 0; i < 3; i++) {
-			value[i] = TSC_IOGxCR(i+1);
-			history[(1000-1)][i] = value[i];
-
-			max[i] = 0;
-			for (uint16_t h = 0; h < (1000 - 1); h++) {
-				if (max[i] < history[h][i])
-					max[i] = history[h][i];
-
-				history[h][i] = history[h+1][i];
-			}
-
-			max[0] = 3043;
-			max[1] = 3452;
-			max[2] = 3651;
-
-			value[i] = max[i] - value[i];
-			if(value[i] > 1000)
-				valid = 0;
-		}
-
-		if(valid)
-			printf("%5u %5u %5u\n", value[0]*170/100, value[1], value[2]);
-
-		if ((value[0]*140/100)>140)
-			gpio_set LED_RED;
-		else
-			gpio_clear LED_RED;
-
-		if (value[1]>140)
-			gpio_set LED_ORANGE;
-		else
-			gpio_clear LED_ORANGE;
-
-		if (value[2]>140)
-			gpio_set LED_GREEN;
-		else
-			gpio_clear LED_GREEN;
-
-
-
-// 		msleep(500);
+	while(1){
+		printf(".\n");
+		msleep(500);
 	}
 
 	return 0;
