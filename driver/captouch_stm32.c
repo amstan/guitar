7ea857b311832293ca8cf722bc49620aebfd7448
Alexandru M Stan <alex@hypertriangle.com>
2017-03-16 21:29:37 -0700

    captouch: better, long history

--- driver/captouch_stm32.c
+++ driver/captouch_stm32.c
@@ -26,9 +26,7 @@
 #error Board not supported
 #endif
 
-#define MAXHISTORY 50
-uint16_t history[MAXHISTORY][TOUCH_NUM];
-uint16_t *current;
+uint16_t *current_touch_values;
 
 void touch_init(void) {
 	/* Remux GPIOs */
@@ -53,11 +51,6 @@ void touch_init(void) {
 #endif
 
 	TSC_CR |= TSC_CR_TSCE; //enable touch sensing controller
-
-	//get some history
-// 	for(i=0; i<100; i++) {
-// 		touch_read();
-// 	}
 }
 DECLARE_HOOK(HOOK_INIT, touch_init, HOOK_PRIO_INIT_CAPTOUCH);
 
@@ -76,6 +69,10 @@ void touch_group_read(uint32_t channels) {
 }
 
 uint16_t* touch_read(void) {
+	#define HISTORY_SIZE 20 /* about 10 minutes */
+	const size_t switch_counter_max = 6000; /* about 600ms */
+	static uint32_t switch_counter;
+	static uint16_t history[HISTORY_SIZE][TOUCH_NUM];
 	uint16_t temp_values[TOUCH_NUM];
 	static uint16_t return_values[TOUCH_NUM];
 	unsigned int i, h;
@@ -102,24 +99,30 @@ uint16_t* touch_read(void) {
 #error Board not supported
 #endif
 
-	for(i = 0; i < TOUCH_NUM; i++) {
-		uint8_t update = ABS(history[(MAXHISTORY-1)][i] - temp_values[i]) > 10;
-		if (update)
-			history[(MAXHISTORY-1)][i] = temp_values[i];
+	switch_counter++;
+	if (switch_counter > switch_counter_max)
+		switch_counter = 0;
 
-		max[i] = 0;
-		for (h = 0; h < (MAXHISTORY - 1); h++) {
-				if (max[i] < history[h][i])
-						max[i] = history[h][i];
-				if (update)
-					history[h][i] = history[h+1][i];
+	for(i = 0; i < TOUCH_NUM; i++) {
+		/* keep track of max on the last entry */
+		uint16_t *last_touch_entry = &history[(HISTORY_SIZE - 1)][i];
+		if (*last_touch_entry < temp_values[i])
+			*last_touch_entry = temp_values[i];
+
+		/* If there's a switch, advance the history too */
+		if (switch_counter == 0) {
+			for (h = 0; h < (HISTORY_SIZE - 1); h++)
+					history[h][i] = history[h + 1][i];
+			history[HISTORY_SIZE - 1][i] = temp_values[i];
 		}
+
+		/* find the max in the history */
+		max[i] = 0;
+		for (h = 0; h < HISTORY_SIZE; h++)
+			if ((max[i] < history[h][i]))
+				max[i] = history[h][i];
+
 		temp_values[i] = max[i] - temp_values[i];
-		if(temp_values[i] > 32000)
-				temp_values[i] = 0;
-		temp_values[i] /= 6;
-		if(temp_values[i] > 255)
-			temp_values[i] = 255;
 		return_values[i] = temp_values[i];
 	}
 
@@ -129,13 +132,10 @@ uint16_t* touch_read(void) {
 static int captouch_read(int argc, char **argv)
 {
 	uint8_t i;
-	uint16_t *values;
 	ccprintf("Reading %d pads: ", TOUCH_NUM);
 
-	values = current;
-
 	for (i = 0; i < TOUCH_NUM; i++) {
-		ccprintf("0x%04x ", values[i]);
+		ccprintf("0x%04x ", current_touch_values[i]);
 	}
 	ccprintf("\n");
 
@@ -148,11 +148,8 @@ DECLARE_CONSOLE_COMMAND(touchread, captouch_read,
 int hc_captouch_raw(struct host_cmd_handler_args *args)
 {
 	uint16_t *p = args->response;
-	uint16_t *values;
 
-	values = current;
-
-	memcpy(p, values, TOUCH_NUM * 2);
+	memcpy(p, current_touch_values, TOUCH_NUM * 2);
 	args->response_size = TOUCH_NUM * 2;
 
 	return EC_RES_SUCCESS;
@@ -166,7 +163,7 @@ void captouch_task(void)
 	CPRINTS("captouch task starting");
 
 	while (1) {
-		current = touch_read();
+		current_touch_values = touch_read();
 		task_wait_event(10000);
 	}
 }
